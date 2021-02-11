67bee5051ee6432d18a3888258e1303af913d115
cchinolla <cmchinolla@miners.utep.edu>
2021-02-11 15:57:22 -0700

    created user interface

---
+++ src/ui.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define LIMIT 100
+char str[LIMIT];
+
+
+int main(){
+  // char str[100];
+  printf("Enter a String or press ENTER to exit\n");
+  int x = 1;
+  int i = 0;
+  while(x){
+    char input;
+    putchar('$');
+    for(input; (input = getchar()) != '\n' && i < LIMIT; i++){
+      if(input == '0'){
+	x = 0;
+	break;
+      }
+      str[i] = input;
+      putchar(input);
+      
+    }
+      putchar('\n');
+    
+  }
+  
+}
