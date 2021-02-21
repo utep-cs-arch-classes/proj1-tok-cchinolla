23192ef7d2077c02aef5332b2a77bed19f1cd745
cchinolla <cmchinolla@miners.utep.edu>
2021-02-20 19:57:15 -0700

    initiallized second mile stone functions

---
+++ src/tokenizer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+
+//true if c is a tab or space, and not zero
+int space_char(char c){
+  
+}
+
+
+//true if c not a tab or space, and not zero
+int non_space_char(char c){
+
+}
+
+//* to first non-space char in first word
+char *word_start(char *str){
+
+}
+
+//* to for space char after end of word
+char *word_end(chaar *str){
+
+}
+
+//the number of words in s
+int count_words(char *str){
+
+}
