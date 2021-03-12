1e6f3a90d2b6ad8d6b6573d6cc2a62d19cc98df0
cchinolla <cmchinolla@miners.utep.edu>
2021-03-12 00:49:49 -0700

    created word start and word end methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -4,50 +4,89 @@
 
 //true if c is a tab or space, and not zero
 int space_char(char c){
-  if ((c != 0 ) && c == " " | c == "\t"){
-    return true;
+  if ((c != '\0' ) && c == ' ' | c == '\t'){
+    return 1;
   }else{
-    return false;
+    return 0;
 }
 
 
 //true if c not a tab or space, and not zero
 int non_space_char(char c){
-  if ((c != 0) && c != " " | c != "\t"){
-    return true;
+  if ((c != '\0') && c != " " | c != "\t"){
+    return 1;
   }else{
-    return false;
+    return 0;
 }
 
 //* to first non-space char in first word
 char *word_start(char *str){
   int i = 0;
-  while(non_space_char(word[i])){
-    i++;
-  }
-}
-
-//* to for space char after end of word
-char *word_end(chaar *str){
-  int i = 0;
-  while(non_space_char(str[i])){
+  while(space_char(str[i])){
     i++;
   }
   return &str[i];
 }
 
+//* to for space char after end of word
+char *word_end(chaar *word){
+  int i = 0;
+  while(non_space_char(word[i])){
+    i++;
+  }
+  return &word[i];
+}
+
 //the number of words in s
-int count_words(char *str){
+int count_words(char *s){
   int count = 0;
-  int count2 = 0;
-  while(*str){
-    if(*str == '/t' || *str == ' ' || *str == '\n'){
-      count 2 = 0;
-    }else if(count2 == 0){
-      count2 = 1;
-      count++;
-    }
-    str++;
+  int i = 0;
+  while(s != word_end(s)){
+    s = word_end(s);
+    count++;
   }
   return count;
 }
+
+ char *copy_str(char *inStr, short len){
+   char *outputString = malloc((len +1) * sizeof(char));
+   int i = 0;
+   while (i <= len){
+     outputString[i] = inStr[i];
+     i++;
+   }
+   return outputString;
+ }
+
+ char **tokenize(char *str){
+   int index, len;
+   int totalIndex = count_words(str);
+   char **tokens = malloc((totalIndex + 1) *sizeof(char *));
+   char *wordStart = word_start(str);
+   char *wordEnd = word_end(str);
+
+   for(i = 0; i < totalIndex; i++){
+     tokens[i] = copy_str(wordStart, wordEnd-wordStart);
+     wordStart = word_start(wordEnd);
+     wordEnd = word_end(wordStart);
+   }
+   tokens[i] = 0;
+   return tokens;
+ }
+
+ void print_tokens(char **tokens){
+   int i = 0;
+   while(tokens[i] != NULL){
+     printf("%s\n", tokens[i]);
+     index++;
+   }
+ }
+
+ void free_tokens(char **tokens){
+   int i = 0;
+   while(tokens[i] != NULL){
+     free(tokens[i]);
+     i++;
+   }
+   free(tokens);
+ }
