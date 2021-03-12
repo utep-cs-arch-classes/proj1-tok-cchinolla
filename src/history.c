caa2fa74d63098b81e75f67dc4cdea46b9f7d1ef
cchinolla <cmchinolla@miners.utep.edu>
2021-03-12 02:15:00 -0700

    created add and get history functions

--- src/history.c
+++ src/history.c
@@ -9,3 +9,39 @@ List* init_history(){
   return list;
 }
 
+void add_history(List *list, char *str){
+  Item *item = malloc(sizeof(Item));
+  Item *temp = list->root;
+
+  item->str = str;
+  item->next = NULL;
+  int count = 0;
+
+  if(list->root == NULL){
+    item->id = count;
+    list->root = item;
+  }else{
+    count = 1;
+    while (temp->next != NULL){
+      count++;
+      temp = temp->next;
+    }
+    temp->next = item;
+    item->id = count;
+  }
+}
+
+char *get_history(List *list, int id){
+  if(list->root == NULL){
+    printf("list in empty");
+  }else{
+    Item *current = list-.root;
+    while(current->next != NULL){
+      if(current->id == id){
+	return current->str;
+      }
+      current = current->next;
+    }
+  }
+}
+
